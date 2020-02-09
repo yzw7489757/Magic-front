6abcc5666e5c2c37e235b56e1f3d84a7b992d667
SevenYzw <yuanziwen7489757@gmail.com>
2020-02-10 03:40:25 +0800

    chore:(PUBLIC_URL)

--- applications/performance/src/public-path.js
+++ applications/performance/src/public-path.js
@@ -1,7 +1,5 @@
-const { name } = require('../package');
-
 if (window.__POWERED_BY_QIANKUN__) {
-  window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = `/${name}/`;
+  window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = `${process.env.PUBLIC_URL}/`;
   // eslint-disable-next-line no-undef
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
 }
