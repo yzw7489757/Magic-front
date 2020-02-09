69e257a72016bd47cb9253c42a3ec02ad22f9739
SevenYzw <yuanziwen7489757@gmail.com>
2020-02-10 03:33:48 +0800

    chore:(Defect domain)

--- applications/appManagement/src/public-path.js
+++ applications/appManagement/src/public-path.js
@@ -1,4 +1,7 @@
+const { name } = require('../package')
+
 if (window.__POWERED_BY_QIANKUN__) {
   // eslint-disable-next-line no-undef
+  window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = '/' + name
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
 }
