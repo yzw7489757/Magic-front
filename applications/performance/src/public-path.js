5b552b4290a9d41f16ecf78c1a4a8e789d47c1e5
SevenYzw <yuanziwen7489757@gmail.com>
2020-02-10 03:38:17 +0800

    chore:(Defect domain)

--- applications/performance/src/public-path.js
+++ applications/performance/src/public-path.js
@@ -1,4 +1,7 @@
+const { name } = require('../package');
+
 if (window.__POWERED_BY_QIANKUN__) {
+  window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = `/${name}/`;
   // eslint-disable-next-line no-undef
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
 }
