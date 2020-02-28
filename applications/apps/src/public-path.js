78bf39d8b9d0d512b49b87a284fa9c22ae85ba6e
yzw7489757 <yuanziwen7489757@gmail.com>
2020-02-28 10:16:36 +0800

    Update public-path.js

--- applications/apps/src/public-path.js
+++ applications/apps/src/public-path.js
@@ -5,7 +5,7 @@ const domain = process.env.VUE_APP_DOMAIN
 
 if (window.__POWERED_BY_QIANKUN__) {
   if (IS_PROD) {
-    window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = `/${name}` // `${protocol}://${domain}/`
+    window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = `${protocol}://${domain}/`
   }
   // eslint-disable-next-line no-undef
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
