a6673d5dc258788384b9437cddcd48b03c867f86
yzw7489757 <yuanziwen7489757@gmail.com>
2020-02-28 10:16:09 +0800

    Update public-path.js

--- applications/performance/src/public-path.js
+++ applications/performance/src/public-path.js
@@ -5,7 +5,7 @@ import { name } from '../package.json'
 
 if (window.__POWERED_BY_QIANKUN__) {
   if (IS_PROD) {
-    window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = `/${name}` // `${protocol}://${domain}/`;
+    window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = `${protocol}://${domain}/`;
   }
   // eslint-disable-next-line no-undef
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
