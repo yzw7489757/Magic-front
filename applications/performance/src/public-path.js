a493c89a67d462fd368bd348a94ed3ec82c65a3d
yzw7489757 <yuanziwen7489757@gmail.com>
2020-02-28 10:12:31 +0800

    Update public-path.js

--- applications/performance/src/public-path.js
+++ applications/performance/src/public-path.js
@@ -1,10 +1,11 @@
 const IS_PROD = process.env.NODE_ENV === 'production'
 const protocol = process.env.REACT_APP_PROTOCOL
 const domain = process.env.REACT_APP_DOMAIN
+import { name } from '../package.json'
 
 if (window.__POWERED_BY_QIANKUN__) {
   if (IS_PROD) {
-    window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = `${protocol}://${domain}/`;
+    window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = `/${name}` // `${protocol}://${domain}/`;
   }
   // eslint-disable-next-line no-undef
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
